edf88e22b179900e498520b90b4bbee9e711b30c
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-10-04 14:43:53 +0530

    CountTo N

---
+++ countToN.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter the number"<<endl;
+    cin>>num;
+
+    for(int i = 0; i<=num; i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
