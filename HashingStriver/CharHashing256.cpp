d02c719bf78b3b9cdf08fb60d043f8667c56638b
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-10-08 23:00:18 +0530

    AllCharHashing256

---
+++ HashingStriver/CharHashing256.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        string s;
+    cout << "Enter the string" << endl;
+    cin >> s;
+   
+    
+
+    // precompute:
+    int hash[256] = {0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i]]++;
+        //no need to subtract to get the corresoponding hash array address
+        //as it already contains 0 - 255 (all characters)
+        
+    }
+
+    cout << "Enter queries" << endl;
+    int q;
+    cin >> q;
+    cout << "Enter number who frequency is required" << endl;
+    while (q--)
+    {
+        char c;
+        cin >> c;
+        // fetching:
+        cout << hash[c] << endl;  
+    }
+    return 0;
+}
