f49865ebffcc529f7d87ebd7bd68814d407af701
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-10-11 13:31:47 +0530

    insertion

--- Sorting/Insertion.cpp
+++ Sorting/Insertion.cpp
@@ -1,33 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void InsertionSort(int arr[], int n){
-    for(int i =0; i<n; i++){
+void InsertionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         int temp = arr[i];
-        int j = i-1; //j is running reverse from i-1 to 0 
+        int j = i - 1; // j is running reverse from i-1 to 0
 
-         for(; j>=0; j--){  // Inner loop to find the correct position for the current element by comparing it with the previous elements.
-            if(arr[j] > temp){ // If the previous element is greater than the current element, shift the previous element to the right.
+        for (; j >= 0; j--)
+        { // Inner loop to find the correct position for the current element by comparing it with the previous elements.
+            if (arr[j] > temp)
+            { // If the previous element is greater than the current element, shift the previous element to the right.
 
-                arr[j+1] = arr[j];  // If the condition is true, it shifts the element at index 'j' one position to the right in the array.
-
+                arr[j + 1] = arr[j]; // If the condition is true, it shifts the element at index 'j' one position to the right in the array.
             }
-            else{
-                break; //nothing to do as element is already greater then temp
+            else
+            {
+                break; // nothing to do as element is already greater then temp
             }
-         }
-         arr[j+1] = temp;
+        }
+        arr[j + 1] = temp; // insert the element that shifted position
     }
 }
 int main()
 {
-    
-    
-    int arr[7] = {3,12,6,21,3,176,4};
+
+    int arr[7] = {3, 12, 6, 21, 3, 176, 4};
     InsertionSort(arr, 7);
 
-  for(int i=0; i< 7; i++){
-    cout<<arr[i]<<endl;
-  }
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << endl;
+    }
     return 0;
 }
