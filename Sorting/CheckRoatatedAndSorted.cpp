8688adad6188aa9a7f7a0da330e62f2d25057574
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-10-12 16:23:17 +0530

    corrected checkSortedrotated

--- Sorting/CheckRoatatedAndSorted.cpp
+++ Sorting/CheckRoatatedAndSorted.cpp
@@ -6,23 +6,25 @@ bool CheckSortedRotated(int arr[], int n){
     int count = 0; // initialized count to zero 
     
     for(int i=1 ; i< n; i++){
-        if(arr[i-1] < arr[i]){    //check if ascending order of the sorted array breaks if breaks increment the counter by 1;
+        if(arr[i-1] > arr[i]){    //check if ascending order of the sorted array breaks if breaks increment the counter by 1;
             count++;
         }
 
         //check for the last element
-        if(arr[n-1] > arr[0]){ 
+        
+    }
+    if(arr[n-1] > arr[0]){ 
             count++;
         }
-    }
-    return count <=1;
+        return count<=1;
+    
 }
 int main()
 {
      int arr[6] = {1,2,3,4,5,6};
     int n = 6;
-    CheckSortedRotated(arr, n);
-    
+   cout<< CheckSortedRotated(arr, n);
+
     
     return 0;
 }
