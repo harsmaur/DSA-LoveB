ce56d1757b9bed99e2fd6dbede2800e1ca15acfe
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-10-15 11:16:51 +0530

    Max Occur Char

--- Char Arrays and Strings/MaxOccuringChar.cpp
+++ Char Arrays and Strings/MaxOccuringChar.cpp
@@ -3,14 +3,41 @@ using namespace std;
 
 char MaxOccuringChar(string a){
     int arr[26] = {0};
+    
+//createing an arraow of count
 
     for(int i=0; i<a.length(); i++){
-        
+        char ch = a[i];
+       int number = 0;
+       //if in lowercase
+       if(ch >='a' && ch <='z'){
+         number = ch-'a';
+       }
+       else{ // if in uppercase
+        number = ch-'A';
+       }
+       arr[number]++;
     }
+
+int maxi  = -1;
+int ans = 0;
+for(int i=0; i<26; i++){
+    if(maxi < arr[i]){
+        ans = i;
+        maxi = arr[i];
+    }
+}
+char fimalAns = 'a' + ans;
+return fimalAns;
+
+
+
+
 }
 int main()
 {
     string s;
     cin>>s;
+    cout<<MaxOccuringChar(s);
     return 0;
 }
