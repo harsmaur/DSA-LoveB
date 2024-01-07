12db93c7621baa22d00564ef87b2d2e445c23297
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-07 14:20:46 +0530

    arraySum Recursion

--- Recursion/SumOfArray.cpp
+++ Recursion/SumOfArray.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int arraySum(int *arr, int size){
+    if(size == 0){
+        return 0;
+    }
+
+    return arr[size-1] + arraySum(arr, size-1);
+   
+}
+int main(){ 
+
+    int arr[5] = {1,2,7,8,8};
+    int size= 5;
+
+    int sum = arraySum(arr, size);
+    cout<<"Sum of array is "<<sum<<endl;
     
     return 0;
 }
