03de8ed02efea1017dd3698407cdba8f138613d6
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-07 16:04:30 +0530

    reverse string

---
+++ Recursion/ReverseString.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void reverseString(string &str, int s, int e){
+    if(s>e){
+        return ;
+    }
+
+    swap(str[s], str[e]);
+    s++;
+    e--;
+    reverseString(str, s, e);
+}
+int main()
+{
+    string str = "abcde";
+    reverseString(str, 0, str.length()-1);
+    cout<<str<<endl;
+    return 0;
+}
