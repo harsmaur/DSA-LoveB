cd57a11464de306cc636626abc0ec5c761be5c48
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-17 18:14:51 +0530

    recursive reverse

--- LinkedLists/ReverseLinkedList.cpp
+++ LinkedLists/ReverseLinkedList.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//iterative solution 
 Node * reverseLinkedList(Node * head){
     if(head == NULL || head->next == NULL){
         return head;
@@ -17,6 +18,19 @@ Node * reverseLinkedList(Node * head){
         curr = forward;
     }
 }
+
+void reverseRecursive(Node * head, Node * curr, Node * prev ){
+    if(curr == NULL){
+        head = prev;
+        return;
+    }
+    Node * forward = curr->next;
+    reverseRecursive(head, forward, curr);
+    curr->next = prev;
+
+}
+
+
 int main()
 {
     
