91fef539107e2aea564a605321c3195ceadbe87b
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-21 18:04:31 +0530

    refactored code for deletion of duplicate in singly list

--- LinkedLists/DuplicateRmoveSortedSiblyLL.cpp
+++ LinkedLists/DuplicateRmoveSortedSiblyLL.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
 
+
 //   Definition for singly-linked list.
   struct ListNode {
       int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
       ListNode(int x) : val(x), next(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
-
+ 
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -20,8 +21,12 @@ public:
 
         ListNode * curr = head;
         while(curr != NULL && curr->next != NULL){
+
             if(curr->val == curr->next->val){
-                curr->next = curr->next->next;
+                ListNode * next_next = curr->next->next;
+                ListNode * NodeToDelete = curr->next;
+                delete(NodeToDelete);
+                curr->next = next_next;
             }else{
                 curr = curr->next;
             }
