3f43f9adf544ca928075a2e059d8de8cff41eb16
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-23 00:32:59 +0530

    refactored code for approach 2

--- LinkedLists/Sort0s1s2sApproach2.cpp
+++ LinkedLists/Sort0s1s2sApproach2.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 
-
 // Following is the class structure of the Node class:
-
 class Node
 {
 public:
@@ -26,48 +24,56 @@ public:
     }
 };
 
-
-void insertATTail(Node * &tail, Node* curr){
+// Function to insert a node at the tail of a linked list
+void insertATTail(Node*& tail, Node* curr)
+{
     tail->next = curr;
     tail = curr;
 }
-Node* sortList(Node *head){
-      
-      Node * oneHead = new Node(-1);
-      Node * oneTail = oneHead;
 
-      Node * twoHead = new Node(-1);
-      Node * twoTail = twoHead;
+// Function to sort a linked list containing values 0, 1, and 2
+Node* sortList(Node* head)
+{
+    // Create dummy nodes for three different values
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
 
-      Node * zeroHead = new Node(-1);
-      Node * zeroTail = zeroHead;
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
 
-        Node * curr = head;
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
 
+    // Traverse the original linked list and segregate nodes based on values
+    Node* curr = head;
 
-        while(curr != NULL){
-            int value = curr->data;
-            if(value == 0){
-                insertATTail(zeroTail, curr);
-            }
-           else if(value == 1){
-               insertATTail(oneTail, curr);
-            }
-            else if(value == 2){
-                insertATTail(twoTail, curr);
-            }
-            curr = curr->next;
+    while (curr != NULL)
+    {
+        int value = curr->data;
+        if (value == 0)
+        {
+            insertATTail(zeroTail, curr);
         }
+        else if (value == 1)
+        {
+            insertATTail(oneTail, curr);
+        }
+        else if (value == 2)
+        {
+            insertATTail(twoTail, curr);
+        }
+        curr = curr->next;
+    }
 
+    // Merge the three sublists
+    zeroTail->next = (oneHead->next != NULL) ? oneHead->next : twoHead->next;
+    oneTail->next = twoHead->next;
+    twoTail->next = NULL;
 
-        //merge
-        zeroTail->next = (oneHead->next != NULL) ?oneHead->next:twoHead->next;
-       
-
-        oneTail->next = twoHead->next;
-        twoTail->next = NULL;
-
-        return zeroHead->next;
-
-
+    // Return the head of the sorted list
+    head = zeroHead->next;
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+    return head;
 }
