cfae8caf419d734666a61318fbbc51cb5f08ad73
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-22 18:58:15 +0530

    split in two halves circular LL

---
+++ LinkedLists/SplitInTwoHalvesCircular.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h> 
+/********************************
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+********************************/
+Node * getMiddleNode(Node * head){
+    Node * slow = head;
+    Node* fast = head->next; // in circular we start fast from head->next 
+    while(fast != head && fast->next != head){
+       fast = fast->next->next;
+       slow = slow->next;
+    }
+    return slow;
+}
+void splitCircularList(Node *head)
+{
+    Node * slow = getMiddleNode(head);
+    // Write your code here.
+    Node * head1 = head;
+    Node  * head2 = slow->next;
+
+    slow->next = head1;
+
+    Node * curr = head2;
+    while(curr->next !=head){
+        curr = curr->next;
+    }
+    curr->next = head2;
+
+
+}
