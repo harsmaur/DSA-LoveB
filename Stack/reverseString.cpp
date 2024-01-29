81bafea66f0af6ba664c6e1a1d8feb7b1a0c5c66
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-29 15:32:42 +0530

    reverse string stack

---
+++ Stack/reverseString.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       stack<char> ch;
+
+       for(int i =0; i< s.size(); i++){
+           ch.push(s[i]);
+       }
+
+       s.clear();
+       while(!ch.empty()){
+           s.push_back(ch.top());
+           ch.pop();
+       }
+
+
+
+    }
+};
