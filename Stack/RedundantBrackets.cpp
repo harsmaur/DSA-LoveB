ba3d57f3a4ee87827e3e9a4f514cd522b6cdc58e
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-31 15:42:19 +0530

    redundant brackets

---
+++ Stack/RedundantBrackets.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+
+bool findredundant(string &s){
+
+    stack<char> st;
+
+    for(int i= 0;i <s.length(); i++){
+
+        char ch = s[i];
+                // Opening bracket or operator, then push to stack
+        if(ch == '(' || ch == '+' || ch == '-' || ch == '/' || ch == '*'){
+            st.push(ch);
+        }
+        else{
+            //closing bracket or lower case letter( letter ignored)
+            if(ch ==')'){
+                bool redundant = true;
+        
+                //until an opening bracket is found search for a operator
+                while(st.top() != '('){
+                    char top = st.top();
+                      if(top == '(' || top == '+' || top == '-' || top == '/' || top == '*'){
+                       
+                        redundant = false;
+                      }
+                      st.pop()
+                }
+
+                if(redundant){
+                    return true;
+                }
+                st.pop(); // pop the opening bracket
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    
+    return 0;
+}
