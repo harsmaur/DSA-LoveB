b0afb079dc9d2fba1f51b6adc09ae70b2e191c48
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-04-10 16:56:10 +0530

    count leaf nodes

---
+++ Binary Trees/countLeafNodes.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+    
+
+void inorder(BinaryTreeNode<int> *root, int &count){
+    if(root == NULL){
+        return ;
+    }
+    inorder(root->left, count);
+
+    //condition to if cuurent node is leaf node/ check for leaf node
+    if(root->left == NULL && root->right == NULL){
+        count++;
+    }
+
+    inorder(root->right,  count);
+}
+
+
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    int count = 0;
+    inorder(root,  count);
+    return count;
+    
+}
