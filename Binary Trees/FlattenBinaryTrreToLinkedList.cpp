4512218c4aba2bcf297273f6a2ec15accf71f5de
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-05-10 01:23:10 +0530

    flatten bianry tree

---
+++ Binary Trees/FlattenBinaryTrreToLinkedList.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+void flatten(Node * root){
+    Node * curr = root;
+
+    while(curr !=NULL){
+         if(curr->left){
+            Node * prev = curr->left;
+            while(prev->right){
+                prev = prev->right;
+            
+                prev->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            
+            curr=curr->right;
+         }
+    }
+}
+int main()
+{
+    
+    return 0;
+}
