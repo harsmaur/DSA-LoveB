afea0378c627194e6915db4315f7af42b54668e2
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-05-24 15:44:00 +0530

    heap

---
+++ Heap/heap.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+class heap{
+
+    public:
+    int arr[100];
+    int size;
+    
+    heap(){
+        arr[0] = -1;
+        size = 0;
+
+    }
+
+    void insert(int val){
+      size = size+1;
+      int index = size;
+      //insert at value at the end
+      arr[index] = val;
+
+      // check for max heap condition
+      while(index > 1){
+        int parent = index/2;
+         //max heap 
+        if(arr[parent] < arr[index]){
+            swap(arr[parent], arr[index]);
+            index = parent;
+        }
+        else{
+            return;
+        }
+
+      }
+    }
+
+    void print(){
+        for(int i = 1; i<=size; i++){
+            cout<<arr[i]<<" ";
+        }cout<<endl;
+    }
+
+    void deleteFromHeap(){
+        if(size == 0){
+           cout<<" Nothing to delete"<<endl;
+           return;
+        }
+         //step1
+         //last element ko root(first) me dal dia
+        arr[1] = arr[size];
+
+        //step2 remove last element
+        size--;
+
+         //step3
+        //take root node to its correct position
+        int i = 1;
+        while(i<size){
+            int leftIndex = 2*i;
+            int rightIndex = 2*i+1;
+
+            if(leftIndex < size &&arr[i] <  arr[leftIndex]){
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+            else if(rightIndex < size && arr[i] < arr[rightIndex]){
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else{
+                return;
+            }
+        }
+
+
+    }
+
+};
+int main()
+{
+     heap h;
+     h.insert(50);
+     h.insert(55);
+     h.insert(53);
+     h.insert(52);
+     h.insert(54);
+     h.print();
+
+
+     h.deleteFromHeap();
+     h.print();
+
+   
+
+    
+    return 0;
+}
