15915db32eb1a60853a757ebd2c3cf5cd0619424
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-05-25 14:59:15 +0530

    heap sort

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -85,11 +85,11 @@ void heapify(int arr[], int size, int i){
         int left = 2*i;
         int right = 2*i+1;
 
-        if(left < n && arr[largest] < arr[left]){
+        if(left <= n && arr[largest] < arr[left]){
             largest = left;
         }
 
-        if(right < n && arr[largest] < arr[right]){
+        if(right <= n && arr[largest] < arr[right]){
             largest = right;
         }
 
@@ -99,6 +99,19 @@ void heapify(int arr[], int size, int i){
         }
 
 
+}
+
+void heapSort(int arr[], int n){
+    int size = n;
+
+    while(size > 1){
+         // swap 
+         swap(arr[size], arr[1]);
+
+         size--;
+
+         heapify(arr, size, 1);
+    }
 }
 
 
@@ -120,6 +133,8 @@ int main()
 
     int arr[6] = {-1, 54, 53, 55, 52, 50};
     int n =5;
+
+    //heap creation
     for(int i=n/2; i>0; i--){
         heapify(arr , n, i );
     }
@@ -130,7 +145,13 @@ int main()
         cout<<arr[i] <<" ";
     }cout<<endl;
    
+    //heap sort
+    heapSort(arr, n);
+     cout<<"Printing the heap sorted array array"<<endl;
 
+    for(int i=1; i<=n; i++){
+        cout<<arr[i] <<" ";
+    }cout<<endl;
     
     return 0;
 }
