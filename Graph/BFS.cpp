ed6f5523112a4867cad1204d8a8f0eceb77d2780
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-05-29 14:17:42 +0530

    bfs traversal

---
+++ Graph/BFS.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+vector<int> bfstraversal(int V, vector<int> adj[]){
+    int visi[V] = {0};
+
+    visi[0] = 1;
+    queue<int> q;
+    q.push(0);
+
+    vector<int> bfs;
+
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+
+        bfs.push_back(node);
+
+        for(auto it: adj[node]){
+            if(!visi[it]){
+                visi[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
+}
+int main()
+{
+    
+    return 0;
+}
