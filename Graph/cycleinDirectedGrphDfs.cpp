cfeade28726979a33001f9e2f676677a9d8b8a50
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-06-03 02:48:00 +0530

    kahns algo

---
+++ Graph/cycleinDirectedGrphDfs.cpp
@@ -0,0 +1,44 @@
+
+class Solution {
+  
+  private:
+  bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[]){
+      
+      vis[node] = 1;
+      
+      pathVis[node] = 1;
+      
+      //traverse for adajcent nodes
+      for(auto it: adj[node]){
+          
+          //ehrn the node is not visited
+          if(!vis[it]){
+              
+              if(dfsCheck(it, adj, vis, pathVis) == true) return true;
+              
+          } 
+          // if the node has been prviously visited 
+          //but it has to be visited on the same path
+          else if(pathVis[it]){
+              return true;
+          }
+      }
+      pathVis[node] = 0;
+      return false;
+  }
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        int vis[V]  {0};
+        int pathVis[V] = {0};
+        
+        for(int i=0; i<V ;i++){
+            
+            if(!vis[i]){
+                if(dfsCheck(i, adj ,vis, pathVis) == true) return true;
+            }
+        }
+        return false;
+        
+  }
+};
