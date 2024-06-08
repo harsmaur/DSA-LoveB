65a8b4c44c2e611eda93e3e0e8dc84dbfef22d51
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-06-08 16:40:10 +0530

    bellamn and floyd warshall

---
+++ Graph/floydWarshall.cpp
@@ -0,0 +1,53 @@
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n  = matrix.size();
+	    
+	    for(int i = 0; i<n; i++){
+	          for(int j=0; j<n; j++){
+	              
+	               if(matrix[i][j] == -1){
+	                   matrix[i][j] = 1e9;
+	               }
+	               
+	               if(i==j) matrix[i][j] = 0;
+	          }
+	    }
+	    
+	    
+	    for(int k =0; k<n; k++){
+	        
+	         for(int i=0; i<n; i++){
+	             
+	               for(int j =0; j<n; j++){
+	                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+	               }
+	        
+	               }
+	    
+	        
+	    }
+	    
+	    
+	    //to detect negative cycle
+	   // for(int i=0; i<n; i++){
+	   //     if(matrix[i][i] < 0){
+	   //         //it has a negative cycle
+	   //     }
+	   // }
+	   
+	    
+	    //on returning change to the same
+	      for(int i = 0; i<n; i++){
+	          for(int j=0; j<n; j++){
+	              
+	               if(matrix[i][j] == 1e9){
+	                   matrix[i][j] = -1;
+	               }
+	               
+	          }
+	    }
+	    
+	    
+	}
+};
